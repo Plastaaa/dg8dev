9eb17c97caa7d851741f8c3a1a7197632a73abbb
Plastaaa <80416635+Plastaaa@users.noreply.github.com>
2022-09-29 15:45:07 +0200

    Update rachatcashImg.js

--- components/rachatcashImg.js
+++ components/rachatcashImg.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import imgRachat from "../public/rachatcash.JPG"
+import imgRachat from "../public/rachatcash.jpg"
 
 export default class RachatCash extends React.Component {
 
