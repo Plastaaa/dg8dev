8866944003442fe0dd2348ddcce2401efaa92698
Plastaaa <80416635+Plastaaa@users.noreply.github.com>
2022-10-03 09:04:45 +0200

    FAVICON UPDT

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,11 @@ function MyApp({ Component, pageProps }) {
     <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.1/dist/leaflet.css" />
 
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+    <link rel="manifest" href="/site.webmanifest"/>
+
     <meta name="description" content="DG8Camping-car est un site officiel du groupe David Gerbier proposant des véhicules neufs et d'occasions dans toutes les gammes."/>
     <title>DG8 Camping-car</title>
     <Component {...pageProps}/>
