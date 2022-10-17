f5d03faec7962dbb08ba13e3e2290383700310f9
Plastaaa <80416635+Plastaaa@users.noreply.github.com>
2022-10-17 10:34:00 +0200

    ok

--- pages/_app.js
+++ pages/_app.js
@@ -11,9 +11,7 @@ const ScriptImport = React.lazy(() => import('../components/scriptImport'))
 
 
 
-function MyApp({ Component, pageProps }) {
-  useEffect (() => {ReactGA.pageview("Accueil");}, []);
-  
+function MyApp({ Component, pageProps }) {  
   return <>
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import { useEffect } from "react";
 import HomePage from "../components/home"
 import React from "react"
 
 export default function Home(){
+    useEffect (() => {ReactGA.pageview("Accueil");}, []);
+
     return (
       <div>
         <HomePage/>
