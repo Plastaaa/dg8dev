6ef4e2319ca3b06d6085ba89309e5116784677dd
Plastaaa <80416635+Plastaaa@users.noreply.github.com>
2022-10-20 15:03:35 +0200

    V1.12

--- pages/a-propos.js
+++ pages/a-propos.js
@@ -2,6 +2,7 @@ import NavBar from '../components/navbar'
 import Footer from '../components/footer'
 import React, { useEffect } from 'react';
 import ReactGA from 'react-ga';
+import Groupe from '../components/groupe';
 
 export default function Home(){
     useEffect (() => {ReactGA.pageview("A-propos");}, []);
@@ -11,6 +12,9 @@ export default function Home(){
                 {
                     <NavBar/>
                 }
+            </div>
+            <div>
+                <Groupe/>
             </div>
             <div>
                 <Footer/>
