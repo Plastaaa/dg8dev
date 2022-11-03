b706b71f69e87ed026df553e994f75e45ac8de3f
Plastaaa <80416635+Plastaaa@users.noreply.github.com>
2022-11-03 10:56:08 +0100

    V1.19.7.3

--- components/concessCard.js
+++ components/concessCard.js
@@ -4,38 +4,8 @@ import { slugify } from '../utils/helpers'
 function IsThereDesc(props){
     if(props.desc1 == ""){
         return ("")
-    }else if(props.desc2 == ""){
-        return(props.desc1 + " ")
-    }else if(props.desc3 == ""){
-        return(
-            <div>
-                {props.desc1}
-                    <br></br><br></br>
-                {props.desc2}
-            </div>
-        )
-    }else if(props.desc4 == ""){
-        return(
-            <div>
-                {props.desc1}
-                <br></br><br></br>
-                {props.desc2}
-                <br></br><br></br>
-                {props.desc3}
-            </div>
-        )
-    }else{
-        return(
-            <div>
-                {props.desc1}
-                <br></br><br></br>
-                {props.desc2}
-                <br></br><br></br>
-                {props.desc3}
-                <br></br><br></br>
-                {props.desc4}
-            </div>            
-        )
+    }else {
+        return(<p className="indent-8">{props.desc1}</p>)
     }
 }
 
@@ -47,9 +17,11 @@ export default function ConcessCard(props) {
                     <div className="w-full sm:w-full md:w-1/2 lg:w-1/2 xl:w-1/2">
                         <div className="flex flex-wrap">
                             <div className="w-full sm:w-full md:w-full lg:w-1/2 xl:w-1/2">
-                                <Image src={props.linkIMG} alt="me" layout="responsive" height={150} width={250}/>
+                                <Image alt='Logo concession' src={props.linkIMG} className={'rounded-xl'} objectFit={"contain"} layout={"responsive"} height={200} width={400}/>
+                                {/*
+                                    <Image src={props.linkIMG} alt="me" layout="responsive" height={150} width={250}/>*/}
                             </div>
-                            <div className="w-full sm:w-full md:w-full lg:w-1/2 xl:w-1/2">
+                            <div className="w-full sm:w-full md:w-full lg:w-1/2 xl:w-1/2 pl-4">
                                 <p className="text-xl font-bold">
                                     {props.nom}
                                 </p>
@@ -71,9 +43,6 @@ export default function ConcessCard(props) {
                     <div className="w-full sm:w-full md:w-1/2 lg:w-1/2 xl:w-1/2">
                         <IsThereDesc 
                             desc1={props.desc1}
-                            desc2={props.desc2}
-                            desc3={props.desc3}
-                            desc4={props.desc4}
                         />
                     </div>
                 </div>
--- components/cu.js
+++ components/cu.js
@@ -23,40 +23,148 @@ function Stock(props){
     );
 }
 
-function IsThereDesc(props){
-    if(props.desc1 == ""){
-        return ("")
-    }else if(props.desc2 == ""){
-        return(props.desc1 + " ")
-    }else if(props.desc3 == ""){
-        return(
-            <div className='sm:pt-4 pt-4 px-16'>
-                {props.desc1}
-                    <br></br><br></br>
-                {props.desc2}
-            </div>
-        )
-    }else if(props.desc4 == ""){
-        return(
-            <div className='sm:pt-4 pt-4'>
-                {props.desc1}
-                <br></br><br></br>
-                {props.desc2}
-                <br></br><br></br>
-                {props.desc3}
+function IsThereHoraire(props){
+    const lundiEte = props.lundiEte;
+
+    var lundi = props.lundi;
+    var mardi = props.mardi;
+    var mercredi = props.mercredi;
+    var jeudi = props.jeudi;
+    var vendredi = props.vendredi;
+    var samedi = props.samedi;
+    var dimanche = props.dimanche;
+
+    var lundiE = props.lundiE;
+    var mardiE = props.mardiE;
+    var mercrediE = props.mercrediE;
+    var jeudiE = props.jeudiE;
+    var vendrediE = props.vendrediE;
+    var samediE = props.samediE;
+    var dimancheE = props.dimancheE;
+
+    var dateEte = props.dateEte;
+    var dateHiver = props.dateHiver;
+
+    if(lundiEte == ""){
+        return (
+            <div className='w-full'>
+                {/*
+                    <h2 className="mb-4 pt-4 text-xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Hiver</h2>
+                
+                    <p className="font-light text-center text-gray-500 dark:text-gray-400 sm:text-l">{concess.dateHiver}.</p>
+                */}
+                <br></br>
+                <ul>
+                    <li><p className='text-center'>Lundi : {lundi}</p></li>
+                    <li><p className='text-center'>Mardi : {mardi}</p></li>
+                    <li><p className='text-center'>Mercredi : {mercredi}</p></li>
+                    <li><p className='text-center'>Jeudi : {jeudi}</p></li>
+                    <li><p className='text-center'>Vendredi : {vendredi}</p></li>
+                    <li><p className='text-center'>Samedi : {samedi}</p></li>
+                    <li><p className='text-center'>Dimanche : {dimanche}</p></li>
+                </ul>
             </div>
         )
     }else{
         return(
-            <div className='sm:pt-4 pt-4'>
-                {props.desc1}
-                <br></br><br></br>
-                {props.desc2}
-                <br></br><br></br>
-                {props.desc3}
-                <br></br><br></br>
-                {props.desc4}
-            </div>            
+            <div className='flex flex-wrap'>
+                <div className='w-full sm:w-full md:w-full xl:w-1/2'>
+                        <h2 className="mb-4 pt-4 text-xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Hiver</h2>
+                        <p className="font-light text-center text-gray-500 dark:text-gray-400 sm:text-l">{dateHiver}.</p>
+                    <br></br>
+                    <ul>
+                        <li><p className='text-center'>Lundi : {lundi}</p></li>
+                        <li><p className='text-center'>Mardi : {mardi}</p></li>
+                        <li><p className='text-center'>Mercredi : {mercredi}</p></li>
+                        <li><p className='text-center'>Jeudi : {jeudi}</p></li>
+                        <li><p className='text-center'>Vendredi : {vendredi}</p></li>
+                        <li><p className='text-center'>Samedi : {samedi}</p></li>
+                        <li><p className='text-center'>Dimanche : {dimanche}</p></li>
+                    </ul>
+                </div>
+                <div className='w-full sm:w-full md:w-full xl:w-1/2'>
+                    <h2 className="mb-4 pt-4 text-xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Été</h2>
+                    <p className="font-light text-center text-gray-500 dark:text-gray-400 sm:text-l">{dateEte}.</p>
+                    <br></br>
+                    <ul>
+                        <li><p className='text-center'>Lundi : {lundiE}</p></li>
+                        <li><p className='text-center'>Mardi : {mardiE}</p></li>
+                        <li><p className='text-center'>Mercredi : {mercrediE}</p></li>
+                        <li><p className='text-center'>Jeudi : {jeudiE}</p></li>
+                        <li><p className='text-center'>Vendredi : {vendrediE}</p></li>
+                        <li><p className='text-center'>Samedi : {samediE}</p></li>
+                        <li><p className='text-center'>Dimanche : {dimancheE}</p></li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
+
+function IsThereDesc(props){
+    const desc = props.desc;
+    if(desc == ""){
+        return ("");
+    }else{
+        return(
+            <div>
+                <br></br>
+                <p className='pl-4'>{desc}</p>
+            </div>
+        )
+    }
+}
+
+function IsThereRs(props){
+    const rs2 = props.rs2;
+    const rs3 = props.rs3;
+    const rs4 = props.rs4;
+
+    if(rs2 == ""){
+        return("")
+    }else if(rs3 == "" && rs2 == ""){
+        return("")
+    }else if(rs3 == "" && rs2 == "" && rs4 == ""){
+        return("")
+    }else{
+        return(
+            <ul className='pl-8'>
+                <li><a className='font-bold hover:text-green-700' href={rs2}>{props.libellers2}</a></li>
+                <li><a className='font-bold hover:text-green-700' href={rs3}>{props.libellers3}</a></li>
+                <li><a className='font-bold hover:text-green-700' href={rs4}>{props.libellers4}</a></li>
+            </ul>
+        )
+    }
+}
+
+function IsThereTitle(props){
+    const fonted = props.fonted;
+
+    if(fonted == ""){
+        return ("")
+    }else{
+        return (
+            <div>
+                <br></br>
+                <h2 className="mb-4 text-xl tracking-tight font-bold text-gray-900 dark:text-white">{fonted}</h2>
+            </div>
+        )
+    }
+}
+
+function IsThereDescHref(props){
+    const ref = props.ref;
+    const lib = props.lib;
+    const desc = props.desc;
+
+    if(desc == ""){
+        return('')
+    }else{
+        return(
+            <div>
+                <br></br>
+                <p className='pl-4'>{desc}<a className='font-bold hover:text-green-700' href={ref}>{lib}</a></p>
+            </div>
         )
     }
 }
@@ -108,32 +216,20 @@ export default class ConcessionUnique extends React.Component {
                         <div className='px-8 pb-8 sm:px-16 md:px-24 xl:px-36 '>
                             <div className='flex flex-wrap'>
                                 <div className='w-full sm:w-full md:w-full xl:w-2/3 sm:pt-12 pt-16'>
-                                    <p className='pl-4'>{concess.desc1}</p>
-                                    <br></br>
-                                    <p className='pl-4'>{concess.desc2}</p>
-                                    <br></br>
-                                    <h2 className="mb-4 text-xl tracking-tight font-bold text-gray-900 dark:text-white">{concess.fonted1}</h2>
-                                    <p className='pl-4'>{concess.desc3}</p>
-                                    <br></br>
-                                    <p className='pl-4'>{concess.desc4}</p>
-                                    <br></br>
-                                    <p className='pl-4'>{concess.desc5}<a className='font-bold hover:text-green-700' href={concess.rs1}>{concess.libellers1}</a></p>
-                                    <br></br>
-                                    <h2 className="mb-4 text-xl tracking-tight font-bold text-gray-900 dark:text-white">{concess.fonted2}</h2>
-                                    <p className='pl-4'>{concess.desc6}</p>
-                                    <br></br>
-                                    <h2 className="mb-4 text-xl tracking-tight font-bold text-gray-900 dark:text-white">{concess.fonted3}</h2>
-                                    <p className='pl-4'>{concess.desc7}</p>
-                                    <br></br>
-                                    <h2 className="mb-4 text-xl tracking-tight font-bold text-gray-900 dark:text-white">{concess.fonted4}</h2>
-                                    <p className='pl-4'>{concess.desc8}</p>
-                                    <br></br>
-                                    <p className='pl-4'>{concess.desc9}</p>
-                                    <ul className='pl-8'>
-                                        <li><a className='font-bold hover:text-green-700' href={concess.rs2}>{concess.libellers2}</a></li>
-                                        <li><a className='font-bold hover:text-green-700' href={concess.rs3}>{concess.libellers3}</a></li>
-                                        <li><a className='font-bold hover:text-green-700' href={concess.rs4}>{concess.libellers4}</a></li>
-                                    </ul>
+                                    <IsThereDesc desc={concess.desc1}/>
+                                    <IsThereDesc desc={concess.desc2}/>
+                                    <IsThereTitle fonted={concess.fonted1}/>
+                                    <IsThereDesc desc={concess.desc3}/>
+                                    <IsThereDesc desc={concess.desc4}/>
+                                    <IsThereDescHref ref={concess.rs1} lib={concess.libellers1} desc={concess.desc5}/>
+                                    <IsThereTitle fonted={concess.fonted2}/>
+                                    <IsThereDesc desc={concess.desc6}/>
+                                    <IsThereTitle fonted={concess.fonted3}/>
+                                    <IsThereDesc desc={concess.desc7}/>
+                                    <IsThereTitle fonted={concess.fonted4}/>
+                                    <IsThereDesc desc={concess.desc8}/>
+                                    <IsThereDesc desc={concess.desc9}/>
+                                    <IsThereRs rs2={concess.rs2} rs3={concess.rs3} rs4={concess.rs4} libellers2={concess.libellers2} libellers3={concess.libellers3} libellers4={concess.libellers4}/>
                                 </div>
                                 <div className='w-full sm:w-full md:w-full xl:w-1/3 sm:pt-12 pt-16 pb-16'>
                                     <div className='p-8 pr-16'>
@@ -143,45 +239,34 @@ export default class ConcessionUnique extends React.Component {
                                     </div>
                                     <div className='lg:pl-32'>
                                         <div className='p-4 border border-gray-200 rounded-lg shadow-lg'>
-                                            <Image alt='Logo concession' src={concess.linkIMGConcess} className={'rounded-xl'} objectFit={"cover"} layout={"responsive"} height={150} width={400}/>
+                                            <Image alt='Logo concession' src={concess.linkIMGConcess} className={'rounded-xl'} objectFit={"contain"} layout={"responsive"} height={150} width={400}/>
                                         </div>
                                     </div>
                                 </div>
-                                <div className='w-1/2'>
-                                    <h2 className="mb-4 text-2xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Nos horaires</h2>
-                                    <div className='flex flex-wrap'>
-                                        <div className='w-full sm:w-full md:w-full xl:w-1/2'>
-                                            <h2 className="mb-4 pt-4 text-xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Été</h2>
-                                            <p className="font-light text-center text-gray-500 dark:text-gray-400 sm:text-l">{concess.dateEte}.</p>
-                                            <br></br>
-                                            <ul>
-                                                <li><p className='text-center'>Lundi : {concess.lundiEte}</p></li>
-                                                <li><p className='text-center'>Mardi : {concess.mardiEte}</p></li>
-                                                <li><p className='text-center'>Mercredi : {concess.mercrediEte}</p></li>
-                                                <li><p className='text-center'>Jeudi : {concess.jeudiEte}</p></li>
-                                                <li><p className='text-center'>Vendredi : {concess.vendrediEte}</p></li>
-                                                <li><p className='text-center'>Samedi : {concess.samediEte}</p></li>
-                                                <li><p className='text-center'>Dimanche : {concess.dimancheEte}</p></li>
-                                            </ul>
-                                        </div>
-                                        <div className='w-full sm:w-full md:w-full xl:w-1/2'>
-                                            <h2 className="mb-4 pt-4 text-xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Hiver</h2>
-                                            <p className="font-light text-center text-gray-500 dark:text-gray-400 sm:text-l">{concess.dateHiver}.</p>
-                                            <br></br>
-                                            <ul>
-                                                <li><p className='text-center'>Lundi : {concess.lundi}</p></li>
-                                                <li><p className='text-center'>Mardi : {concess.mardi}</p></li>
-                                                <li><p className='text-center'>Mercredi : {concess.mercredi}</p></li>
-                                                <li><p className='text-center'>Jeudi : {concess.jeudi}</p></li>
-                                                <li><p className='text-center'>Vendredi : {concess.vendredi}</p></li>
-                                                <li><p className='text-center'>Samedi : {concess.samedi}</p></li>
-                                                <li><p className='text-center'>Dimanche : {concess.dimanche}</p></li>
-                                            </ul>
-                                        </div>
-                                    </div>
+                                <div className='w-full pb-8 md:pb-0 md:w-1/2'>
+                                    <h2 className="pt-8 mb-4 text-2xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Nos horaires</h2>
                                     
+                                        <IsThereHoraire 
+                                            lundiEte={concess.lundiEte}
+                                            lundi={concess.lundi}
+                                            mardi={concess.mardi}
+                                            mercredi={concess.mercredi}
+                                            jeudi={concess.jeudi}
+                                            vendredi={concess.vendredi}
+                                            samedi={concess.samedi}
+                                            dimanche={concess.dimanche}
+                                            dateEte={concess.dateEte}
+                                            dateHiver={concess.dateHiver}
+                                            lundiE={concess.lundiEte}
+                                            mardiE={concess.mardiEte}
+                                            mercrediE={concess.mercrediEte}
+                                            jeudiE={concess.jeudiEte}
+                                            vendrediE={concess.vendrediEte}
+                                            samediE={concess.samediEte}
+                                            dimancheE={concess.dimancheEte}
+                                        />                                    
                                 </div>
-                                <div className='w-1/2'>
+                                <div className='w-full md:w-1/2'>
                                     <MapBis concess={concess.libelle} latitude={concess.latitude} longitude={concess.longitude}/>
                                 </div>
                             </div>
