d200862d6c5b0beda2568a55118bbaedfa125e24
Plastaaa <80416635+Plastaaa@users.noreply.github.com>
2022-11-15 09:31:44 +0100

    Update carousel.js

--- components/carousel.js
+++ components/carousel.js
@@ -3,7 +3,6 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Image from "next/image";
 import img1 from '../public/slider/slide1.jpg';
-import img2 from '../public/slider/slide2.jpg';
 import "@splidejs/splide/dist/css/splide.min.css";
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 
@@ -19,10 +18,7 @@ export default class SimpleSlider extends Component {
           className={"border border-gray-400 rounded-2xl shadow-xl"}
         >
           <SplideSlide>
-            <Image className="rounded-2xl border border-gray-700" alt="Image Slider" priority={true} src={img1} objectFit={"cover"} layout={"fill"} quality={100}/>
-          </SplideSlide>
-          <SplideSlide>
-            <Image className="rounded-2xl border border-gray-500" alt="Image Slider" priority={true} src={img2} width={800} layout="responsive" height={320} quality={100}/>
+            <Image className="rounded-2xl border border-gray-500" alt="Image Slider" priority={true} src={img1} width={800} layout="responsive" height={320} quality={100}/>
           </SplideSlide>
         </Splide>
       </div>
