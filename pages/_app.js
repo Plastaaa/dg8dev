5e29d70551ee7761582735dd7f4526b9aa96b74d
Plastaaa <80416635+Plastaaa@users.noreply.github.com>
2023-01-04 11:23:55 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,27 @@ const Tidio = React.lazy(() => import('../components/tidio'));
 const ScriptImport = React.lazy(() => import('../components/scriptImport'))
 
 function MyApp({ Component, pageProps }) {  
+  var analyticsCode="G-Y61LMC4L9T"
+
   return <>
+  {/* Global Site Tag (gtag.js) - Google Analytics */}
+    <script
+      async
+      src={`https://www.googletagmanager.com/gtag/js?id=${analyticsCode}`}
+    />
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${analyticsCode}', {
+        page_path: window.location.pathname,
+      });
+    `,
+      }}
+    />
+
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
